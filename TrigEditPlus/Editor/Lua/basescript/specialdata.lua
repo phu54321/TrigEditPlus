538340b520020cf1a6a3dd5587bc7daa7f8b6112
phu54321 <phu54321@naver.com>
2016-03-03 19:29:08 +0900

    removed bit32 dependency

--- TrigEditPlus/Editor/Lua/basescript/specialdata.lua
+++ TrigEditPlus/Editor/Lua/basescript/specialdata.lua
@@ -9,14 +9,14 @@ function inline_eudplib(players, str)
     local playerFlag = 0
     for i = 1, #players do
         local player = EncodePlayer(players[i])
-        playerFlag = bit32.bor(playerFlag, bit32.lshift(1, player))
+        playerFlag = playerFlag | (1 << player)
     end
 
     local playerString = string.char(
-        bit32.extract(playerFlag, 0, 8),
-        bit32.extract(playerFlag, 8, 8),
-        bit32.extract(playerFlag, 16, 8),
-        bit32.extract(playerFlag, 24, 8)
+        (playerFlag) & 0xFF,
+        (playerFlag >> 8) & 0xFF,
+        (playerFlag >> 16) & 0xFF,
+        (playerFlag >> 24) & 0xFF
     )
 
     local callerLine = debug.getinfo(2).currentline
